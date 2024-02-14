e15b454588164d925fe18407ce8afaeb71e97223
sethm3141 <sethmaupin@gmail.com>
2024-02-14 13:13:35 -0600

    finished validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -51,6 +51,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     };
   }
 
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
