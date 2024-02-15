1590b7befed744d90f41d2da0d19f3f4149a610b
sethm3141 <sethmaupin@gmail.com>
2024-02-15 15:26:41 -0600

    small style adjustments to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,7 @@ export const metadata: Metadata = {
     template: '%s | Acme Dashboard',
     default: 'Acme Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: 'Dashboard demonstration.',
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,16 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
           <p
-            className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal`}
+            className={`${lusitana.className} text-xl text-gray-800 antialiased md:text-3xl md:leading-normal text-center`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Acme.</strong> This web app demonstrates
+            multi-page routing, authentication, data-base usage, and
+            full-stack design
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base mx-auto"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
