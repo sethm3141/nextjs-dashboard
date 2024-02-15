c6a6a178d0e4d3d159d9216b5340f78c8e89422f
sethm3141 <sethmaupin@gmail.com>
2024-02-15 15:00:20 -0600

    Finished project tutorial with authentication setup

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import Table from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,7 +72,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
   }
 
   const { customerId, amount, status } = validatedFields.data;
-  const amountInCents = amount * 100;
+  const amountInCents = Math.floor(amount * 100);
   const date = new Date().toISOString().split('T')[0];
 
   try {
@@ -118,7 +118,7 @@ export async function updateInvoice(
   }
 
   const { customerId, amount, status } = validatedFields.data;
-  const amountInCents = amount * 100;
+  const amountInCents = Math.floor(amount * 100);
 
   try {
     await sql`
